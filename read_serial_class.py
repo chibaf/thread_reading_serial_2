f6b49a9243a8817374a3c4d729c858dc9e5f64c8
chibaf <chibaf@mac.com>
2024-11-20 02:38:49 +0900

    Add files via upload

--- read_serial_class.py
+++ read_serial_class.py
@@ -2,7 +2,7 @@ class read_serial():
 
   def read(self,ser,q):
     import serial,time
-    time.sleep(1)
+    time.sleep(0.1)
     line=ser.readline()
     data=line.strip().decode('utf-8',errors='replace')
     q.put(data)
